fbf97dd761f00efb4502b3e4493b5a26162839ee
Nathan Woolley <70766704+woonex@users.noreply.github.com>
2026-04-02 13:31:12 -0500

    docstring and variable update

--- src/services/query/tools/search_manual.py
+++ src/services/query/tools/search_manual.py
@@ -3,17 +3,27 @@ from settings import get_settings
 from qdrant_client import QdrantClient
 from qdrant_client.models import Filter, FieldCondition, MatchValue
 from sentence_transformers import SentenceTransformer
+from langchain.tools import tool
 
 settings = get_settings()
 model = SentenceTransformer(settings.embedding_model)
 qdrant = QdrantClient(url=settings.vector_store_url)
 
-def search_manual(question: str, vehicle: str) -> str:
-    """gets relevant text from owner's manual for the selected vehicle"""
+@tool
+def search_manual(search_info: str, vehicle: str) -> str:
+    """Gets relevant text from owner's manual for a vehicle
+
+    Args:
+        search_info: the text to search for
+        vehicle: the vehicle to search collections for
+
+    Returns:
+        str continaing top matching results from owner's manual
+    """
 
     collection_name = settings.collection_name
     top_k = settings.top_k
-    vector = model.encode(question)
+    vector = model.encode(search_info)
 
     response = qdrant.query_points(
         collection_name=collection_name,
